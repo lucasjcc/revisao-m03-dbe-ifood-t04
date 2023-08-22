c56a94c7efd05667c2082070ab7f8f199af78519
Lucas Carvalho <lucasjorge.cap@gmail.com>
2023-08-22 10:30:51 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Revisão módulo 03
+
+## Descrição da proposta
+Contruir uma RESTful API que simule funcionalidades de aplicativo de gestão financeira de gastos:
+
+- Cadastrar usuário;
+- Fazer login do usuário;
+- O usuário deve ser capaz de exibir seus os gasto;
+- O usuário deve ser capaz de detalhar um gasto;
+- O usuário deve ser capaz de editar um gasto;
+- O usuário deve ser capaz de excluir um gasto.
+- O usuário deve ser capaz de filtrar pelos gastos feitos em um período
+  
+## Banco de dados
+
+### Tabela usuarios
+Essa tabela deve ter os seguintes campos:
+- id (identificador único);
+- nome (obrigatório);
+- email (obrigatório e único);
+- senha (obrigatório).
+
+### Tabela gastos
+Essa tabela deve ter os seguintes campos:
+- id (identificador único);
+- id_usuario (chave estrangeira);
+- valor (maior que zero e obrigatório);
+- data (data do gasto);
+- descricao (optativo).
+
+## Rotas
+
+#### `POST` /`usuarios`
+#### `POST` /`login`
+#### `GET` /`gastos`
+#### `GET` /`gastos/:id`
+#### `PUT` /`gastos/:id`
+#### `DELETE` /`gastos/:id`
+
+
---
+++ bancoDeDados.js
@@ -0,0 +1,96 @@
+const bancoDeDados = {
+    usuarios: [
+        {
+            "id": 1,
+            "nome": "Erich Contreras",
+            "email": "et.netus@icloud.org",
+            "senha": "WKD76LYK7LF"
+        },
+        {
+            "id": 2,
+            "nome": "Oscar Lancaster",
+            "email": "ipsum@outlook.ca",
+            "senha": "SQL35LQR5HD"
+        },
+        {
+            "id": 3,
+            "nome": "Berk Cox",
+            "email": "pede.malesuada@outlook.org",
+            "senha": "WOE51EBN0VK"
+        }
+    ],
+    gastos: [
+        {
+            "id": 1,
+            "id_usuario": 2,
+            "valor": 24.28,
+            "data": "2023-08-24",
+            "descricao": "JKF78KKV2LB6BAH26IEA5GN3SVD51WKF6FS"
+        },
+        {
+            "id": 2,
+            "id_usuario": 1,
+            "valor": 42,
+            "data": "2023-01-01",
+            "descricao": null
+        },
+        {
+            "id": 3,
+            "id_usuario": 1,
+            "valor": 62.96,
+            "data": "2023-04-18",
+            "descricao": "VYR28KMW5VD3VXU51VKC8KL6UPF13PEG9OE"
+        },
+        {
+            "id": 4,
+            "id_usuario": 3,
+            "valor": 54.2,
+            "data": "2023-05-03",
+            "descricao": "UMP36NTB2SM7RZG54JJE2YW4KOK48IVV3CR"
+        },
+        {
+            "id": 5,
+            "id_usuario": 3,
+            "valor": 57.45,
+            "data": "2023-01-23",
+            "descricao": null
+        },
+        {
+            "id": 6,
+            "id_usuario": 2,
+            "valor": 12.22,
+            "data": "2023-04-03",
+            "descricao": "QHK73DQF7HA4JXY74XPN0PW1JQO74IFL9XP"
+        },
+        {
+            "id": 7,
+            "id_usuario": 1,
+            "valor": 91.2,
+            "data": "2023-06-05",
+            "descricao": "XGE20XFR8GV3QXF23CKF6WF6QEF84WBO3IQ"
+        },
+        {
+            "id": 8,
+            "id_usuario": 2,
+            "valor": 77.27,
+            "data": "2023-03-05",
+            "descricao": null
+        },
+        {
+            "id": 9,
+            "id_usuario": 1,
+            "valor": 9,
+            "data": "2023-08-22",
+            "descricao": "WJH12DGI3GC6HIT70NMX7LR8FXV65XYV2UJ"
+        },
+        {
+            "id": 10,
+            "id_usuario": 1,
+            "valor": 21.2,
+            "data": "2023-01-01",
+            "descricao": "ZRX60BBV4GJ6URT53WDF6WP1KGB43ZIB4SL"
+        }
+    ]
+}
+
+module.exports = bancoDeDados
