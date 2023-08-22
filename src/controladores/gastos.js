103aaa8e34e5aadbd7ad9039c01147d7bdda9553
Lucas Carvalho <lucasjorge.cap@gmail.com>
2023-08-22 17:19:14 -0300

    serviço de detalhar gasto criado

--- src/controladores/gastos.js
+++ src/controladores/gastos.js
@@ -4,16 +4,37 @@ const { encontrarGastosPeloId } = require("../banco/gastos")
 const listarGastos = (req, res) => {
     const { id } = req.params
     if (isNaN(Number(id))) {
-        return res.json({ mensagem: "O id deve ser numérico" })
+        return res.status(400).json({ mensagem: "O id deve ser numérico" })
     }
     const pessoaEncontrada = encontrarUsuarioPeloId(id)
     if (!pessoaEncontrada) {
-        return res.json({ mensagem: "Pessoa não encontrada" })
+        return res.status(404).json({ mensagem: "Pessoa não encontrada" })
     }
-    const gastoEncontrados = encontrarGastosPeloId(id)
-    return res.json(gastoEncontrados)
+    const gastosEncontrados = encontrarGastosPeloId(id)
+    return res.status(200).json(gastosEncontrados)
+}
+
+const listarGasto = (req, res) => {
+    const { idPessoa, idGasto } = req.params
+    if (isNaN(Number(idPessoa)) || isNaN(Number(idGasto))) {
+        return res.status(400).json({ mensagem: "O id deve ser numérico" })
+    }
+    const pessoaEncontrada = encontrarUsuarioPeloId(idPessoa)
+    if (!pessoaEncontrada) {
+        return res.status(404).json({ mensagem: "Pessoa não encontrada" })
+    }
+    const gastosEncontrados = encontrarGastosPeloId(idPessoa)
+    if (gastosEncontrados.length === 0) {
+        return res.status(404).json({ mensagem: "Gasto não encontrado" })
+    }
+    const gastoEncontrado = gastosEncontrados.find(gasto => gasto.id === Number(idGasto))
+    if (!gastoEncontrado) {
+        return res.status(404).json({ mensagem: "Gasto não encontrado" })
+    }
+    return res.json(gastoEncontrado)
 }
 
 module.exports = {
-    listarGastos
+    listarGastos,
+    listarGasto
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { listarGastos } = require('./controladores/gastos')
+const { listarGastos, listarGasto } = require('./controladores/gastos')
 
 const rotas = express()
 
 rotas.get('/usuarios/:id/gastos', listarGastos)
+rotas.get('/usuarios/:idPessoa/gastos/:idGasto', listarGasto)
 
 module.exports = rotas
